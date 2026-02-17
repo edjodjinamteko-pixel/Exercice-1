76819b5d7d74c27499e2f598ff0287846c198ec8
Christian <edjodjinam.teko@gmail.com>
2026-02-17 19:04:28 +0100

    Initial commit

---
+++ programme1.py
@@ -0,0 +1,13 @@
+ca = float(input("Entre le chiffre d'affaires : "))
+
+if ca > 5000:
+    taux = 0.07
+else:
+    taux = 0.05
+
+commission = ca * taux
+
+print("\n--- Résultat ---")
+print("CA :", round(ca, 2), "€")
+print("Taux appliqué :", round(taux * 100, 2), "%")
+print("Commission :", round(commission, 2), "€")
